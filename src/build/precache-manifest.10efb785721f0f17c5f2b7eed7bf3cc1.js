5abcc458f653426f60651acc3b60a42309861e0d
lananhdo19 <36138936+lananhdo19@users.noreply.github.com>
2020-06-11 17:22:34 -0400

    first commit

---
+++ src/build/precache-manifest.10efb785721f0f17c5f2b7eed7bf3cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a379a5a50a682290d2ef88a8f9d822a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b2c7084584912ddc8be",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "29f5fbe3254a5a1e7482",
+    "url": "/static/js/2.cd54d20b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd54d20b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2c7084584912ddc8be",
+    "url": "/static/js/main.4fc113f5.chunk.js"
+  },
+  {
+    "revision": "ded462cd9e70cf29fc62",
+    "url": "/static/js/runtime-main.a758755e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
